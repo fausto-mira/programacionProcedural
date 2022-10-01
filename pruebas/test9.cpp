13983892175f4c24c5ac1016326510341551f4bc
Fausto <faustinomirakpo@gmail.com>
2022-10-01 18:22:24 -0300

    01-10-22

---
+++ pruebas/test9.cpp
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct nodo
+{
+    int dato;
+    struct nodo *sig;
+} nodo;
+
+typedef nodo *puntero;
+
+void cargarPorCabeza(puntero &cabeza) // insercion por cabeza
+{
+    int x;
+    printf("Ingrese numero a cargar. termina con 0 \n");
+    scanf("%d", &x);
+    while (x != 0)
+    {
+        puntero nuevo, aux, anterior;
+        nuevo = (puntero)malloc(sizeof(struct nodo)); // creo un nuevo nodo
+        nuevo->dato = x;                              // guardo el dato en el nuevo nodo
+        nuevo->sig = cabeza;                          // le asigno la cabeza al nuevo nodo
+        cabeza = nuevo;                               // la cabeza sera ahora el nuevo nodo
+        printf("Ingrese numero a cargar. termina con 0 \n");
+        scanf("%d", &x);
+    }
+}
+
+void cargaPorCola(puntero &cabeza)
+{
+    puntero nuevo, p;
+    int x;
+    printf("Ingrese numero a cargar. termina con 0 \n");
+    scanf("%d", &x);
+    while (x != 0)
+    {
+        nuevo = (puntero)malloc(sizeof(nodo));
+        nuevo->dato = x;
+        nuevo->sig = NULL;
+        if (cabeza == NULL)
+        {
+            cabeza = nuevo;
+        }
+        else
+        {
+            p = cabeza;
+            while (p->sig != NULL)
+                p = p->sig;
+            p->sig = nuevo;
+        }
+        printf("Ingrese numero a cargar. termina con 0 \n");
+        scanf("%d", &x);
+    }
+}
+
+void busqueda(puntero &cabeza, puntero nuevo)
+{
+    if (cabeza == NULL)
+        cabeza = nuevo;
+    else
+        busqueda(cabeza->sig, nuevo);
+}
+
+void cargaPorColaRecursivo(puntero &cabeza, int x)
+{
+    puts("Ingrese numero a cargar. termina con 0");
+    scanf("%d", &x);
+    if (x != 0)
+    {
+        puntero nuevo, p;
+        nuevo = (puntero)malloc(sizeof(nodo));
+        nuevo->dato = x;
+        nuevo->sig = NULL;
+        busqueda(cabeza, nuevo);
+        cargaPorColaRecursivo(cabeza, x);
+    }
+}
+
+void mostrar(puntero cabeza)
+{
+    if (cabeza != NULL)
+    {
+        printf("%d", cabeza->dato);
+        mostrar(cabeza->sig);
+    }
+}
+
+void liberar(puntero &cabeza, puntero p)
+{
+    if (cabeza != NULL)
+    {
+        p = cabeza;
+        cabeza = cabeza->sig;
+        free(p);
+        liberar(cabeza, NULL);
+    }
+}
+
+int main()
+{
+    puntero cabeza, p;
+    cabeza = NULL;
+    int opcion;
+    // cargarPorCabeza(cabeza);
+    // cargaPorCola(cabeza);
+    cargaPorColaRecursivo(cabeza, 0);
+    mostrar(cabeza);
+    liberar(cabeza, p);
+    mostrar(cabeza);
+    printf("\n\n");
+    return 0;
+}
--- u5/u5_e6.cpp
+++ u5/u5_e6.cpp
@@ -27,6 +27,7 @@ Nota: Para los distintos países se registra una sola vez las distintas especies
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #define P 3
 
 typedef struct nodo
@@ -38,23 +39,25 @@ typedef struct nodo
     nodo *sig;
 } nodo;
 
+typedef nodo *puntero;
+
 typedef struct pais
 {
     char pais[10];
     char continente[10];
-    nodo *cabeza;
+    puntero cabeza;
 } pais;
 
 typedef nodo *puntero;
 
 void inicializacionPaises(pais xpaises[P])
 {
-    strcpy(xpaises[0].pais, "Argentina");
-    strcpy(xpaises[1].pais, "Brasil");
-    strcpy(xpaises[2].pais, "EEUU");
-    strcpy(xpaises[0].continente, "America");
-    strcpy(xpaises[1].continente, "America");
-    strcpy(xpaises[2].continente, "America");
+    strcpy(xpaises[0].pais, "argentina");
+    strcpy(xpaises[1].pais, "brasil");
+    strcpy(xpaises[2].pais, "eeuu");
+    strcpy(xpaises[0].continente, "america");
+    strcpy(xpaises[1].continente, "america");
+    strcpy(xpaises[2].continente, "america");
     xpaises[0].cabeza = NULL;
     xpaises[1].cabeza = NULL;
     xpaises[2].cabeza = NULL;
@@ -69,22 +72,129 @@ void mostrar(pais xpaises[P], int i)
     }
 }
 
-void carga(pais xpaises[P], puntero xespecies)
+void insertar(puntero &cabeza, puntero especie)
 {
-    int i = 0;
-    char palabra[20];
-    char nombre[20];
+    if (cabeza == NULL)
+        cabeza = especie;
+    else
+        insertar(cabeza->sig, especie);
+}
+
+void carga(pais xpaises[P])
+{
+    int i = 0, c;
+    puntero especie;
+    char nombre[30], nomCientifico[30], reino[10];
+    int cant;
     do
     {
-        printf("Carga pais %s \n", xpaises[i].pais);
-        do
+        printf("INGRESO ESPECIES PAIS %s ==============================\n\n", xpaises[i].pais);
+        puts("Ingrese nombre de especie ( finaliza con nombre \"FIN\")");
+        fflush(stdin);
+        fgets(nombre, 30, stdin);
+        while (strncmp(nombre, "FIN", 3) != 0)
         {
-            puts("Ingrese nombre (finaliza con \"FIN\")");
-            gets(nombre);
-
-        } while (strcmp(nombre, "FIN") != 0);
+            puts("Ingrese nombre cinetifico de la especie");
+            fflush(stdin);
+            fgets(nomCientifico, 30, stdin);
+            fflush(stdin);
+            puts("Ingrese reino al que pertenece");
+            fgets(reino, 10, stdin);
+            puts("Ingrese cantidad de ejemplares");
+            scanf("%d", &cant);
+            getchar(); // para no crear errores entre scanf y fgets, se coloca un getchar despues de cada scanf
+            especie = (puntero)malloc(sizeof(nodo));
+            strcpy(especie->nombre, nombre);
+            strcpy(especie->nc, nomCientifico);
+            strcpy(especie->reino, reino);
+            especie->cant = cant;
+            insertar(xpaises[i].cabeza, especie);
+            puts("Ingrese nombre de especie ( finaliza con nombre \"FIN\")");
+            fflush(stdin);
+            fgets(nombre, 29, stdin);
+            // fflush(stdin);
+        }
         i++;
-    } while (i < P);
+    } while (i < 3);
+}
+
+int contador(puntero cabeza, int flora, int &fauna)
+{
+    if (cabeza != NULL)
+    {
+        if (strncmp(cabeza->reino, "flora", 5) == 0)
+            flora += 1;
+        if (strncmp(cabeza->reino, "fauna", 5) == 0)
+            fauna += 1;
+        return flora = contador(cabeza->sig, flora, fauna);
+    }
+    else
+        return (flora);
+}
+
+void contadorFaunaFlora(pais xpaises[P])
+{
+    char pais[10], pais2[10];
+    int resultado = -1, i, flora, fauna = 0;
+    do
+    {
+        puts("Ingrese nombre del pais a buscar");
+        fflush(stdin);
+        fgets(pais, 10, stdin);
+        for (i = 0; i < P; i++)
+        {
+            if (strncmp(pais, xpaises[i].pais, strlen(xpaises[i].pais)) == 0)
+                resultado = i;
+        }
+        if (resultado == -1)
+            puts("Pais no encontrado");
+
+    } while (resultado == -1);
+    puts("Pais encontrado");
+    flora = contador(xpaises[resultado].cabeza, 0, fauna);
+    printf("Cantidad de reino Fauna: %d \n", fauna);
+    printf("Cantidad de reino Flora: %d \n", flora);
+}
+
+// Incrementar en 200 ejemplares la cantidad de la especie con nombre Petiribí (árbol) en Brasil.
+
+void peteribi200(puntero cabeza)
+{
+    if (cabeza != NULL)
+    {
+        if (strncmp(cabeza->nombre, "peteribi", strlen("peteribi")))
+        {
+            cabeza->cant += 200;
+            printf("Peteribi modificado. Nuevo valor: %d \n", cabeza->cant);
+            return;
+        }
+        peteribi200(cabeza->sig);
+    }
+    else
+    {
+        puntero nuevo;
+        nuevo = (puntero)malloc(sizeof(nodo));
+        strcpy(nuevo->nombre, "peteribi");
+        nuevo->cant = 200;
+        cabeza = nuevo;
+        puts("Como no se encontro la especie, la misma fue creada e inicializada en 200");
+    }
+}
+
+// Indicar en el programa principal cantidad de ejemplares de Petiribí (árbol presente en Argentina y Brasil)
+// considerar solamente los ejemplares de los países indicados.
+
+void cantPeteribi(puntero cabeza, char nombre[10])
+{
+    if (cabeza != NULL)
+    {
+        if (strncmp(cabeza->nombre, "peteribi", strlen("peteribi")))
+        {
+            printf("Cantidad Peteribi en %s: %d \n", nombre, cabeza->cant);
+            return;
+        }
+        cantPeteribi(cabeza->sig, nombre);
+    }
 }
 
 int main()
@@ -93,6 +203,11 @@ int main()
     puntero especies;
     inicializacionPaises(paises);
     mostrar(paises, 0);
+    carga(paises);
+    contadorFaunaFlora(paises);
+    peteribi200(paises[1].cabeza);
+    cantPeteribi(paises[0].cabeza, paises[0].pais);
+    cantPeteribi(paises[1].cabeza, paises[1].pais);
     getchar();
     return 0;
 }
